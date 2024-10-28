25e5ce66d45457288a9804d016d4dc9698bb3017
Ana Camila Pantoja <anacamilapantoja26@gmail.com>
2024-10-28 16:58:32 -0300

    primeiro commit

---
+++ src/br/com/anac/notas.java
@@ -0,0 +1,13 @@
+package br.com.anac;
+
+public class notas {
+	public static void main(String args[]) {
+		int nota1 = Integer.valueOf(8);
+		int nota2 = Integer.valueOf(10);
+		int nota3 = Integer.valueOf(5);
+		
+		System.out.println("Nota 1 é:"+" " + nota1);
+		System.out.println("Nota 2 é:"+" " + nota2);
+		System.out.println("Nota 3 é:"+" " + nota3);
+	}
+}
